bc74891a9978367aec2732c3e3e6eae1fd2c73e0
Mavis2103 <taquanvku@gmail.com>
2021-04-17 21:27:20 +0700

    up

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,18 +1,16 @@
 import React from "react";
+import avatar from "../assets/img/avatar.JPG";
 
 const Avatar = ({ size }) => {
   const css = {
+    objectFit: "cover",
     width: size,
     height: size,
     borderRadius: "100%",
   };
   return (
     <section>
-      <img
-        style={css}
-        src="https://image.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg"
-        alt=""
-      />
+      <img style={css} src={avatar} alt="" />
     </section>
   );
 };
